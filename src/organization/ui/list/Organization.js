9d080a86bb01152a9a2598ad0693945e82f3b441
Leeward Bound <l@lwb.co>
2019-02-16 19:56:22 -0400

    ui-tweaks2

--- src/organization/ui/list/Organization.js
+++ src/organization/ui/list/Organization.js
@@ -24,7 +24,6 @@ class Organization extends React.Component {
   }
   componentWillMount() {
     if (this.props.address) {
-      console.log("MAKING NEW ORG CARD", this.props);
       this.contract = getContractAt("VotingOrganization", this.props.address);
       this.contract.getOrgDetails((err, result) =>
         this.setState({
@@ -43,22 +42,17 @@ class Organization extends React.Component {
     return (
       <Card className={classes.card} raised>
         <CardContent>
-          <h3>
-            <b>Title:</b>&nbsp;{this.state.title}
-          </h3>{" "}
-          {}
-          <h3>
-            <b>Description:</b>&nbsp;
+          <h2>
+            <img
+              alt="Contract blockie"
+              src={"https://blockies.shipchain.io/" + address + ".png?size=small"}
+            /> &nbsp;
+            <Link to={`/organization/list/${address}`}>
+              {this.state.title}
+            </Link>
+          </h2>
             {this.state.description}
-          </h3>
-          <h3>
-            <b>Address:</b>{" "}
-          </h3>{" "}
-          <img
-            alt="Contract blockie"
-            src={"https://blockies.shipchain.io/" + address + ".png?size=small"}
-          />
-          <Link to={`/organization/list/${address}`}>{address}</Link>
+          
         </CardContent>
         <CardActions />
       </Card>
