07b33c74c6cb75054ce02389a99a52cfe0923b4e
Leeward Bound <l@lwb.co>
2019-02-15 23:16:24 -0400

    Adding truffle.js updates

--- truffle.js
+++ truffle.js
@@ -1,4 +1,20 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var process = require("process")
+var mnemonic = process.env.MNEMONIC;
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic,
+          "https://rinkeby.infura.io/v3/72b574db820841cab063fd946c07a5fa");
+      },
+      network_id: 1
+    }
+  }
 };
