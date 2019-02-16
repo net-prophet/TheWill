13261a21c6f5ea86fe402be1813c1a39a90215be
Leeward Bound <l@lwb.co>
2019-02-16 15:03:35 -0400

    Building homepage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router'
 import { Provider } from 'react-redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { UserIsAuthenticated } from './util/wrappers.js'
-
+import {createHistory} from 'history'
 // Layouts
 import App from './App'
 import Home from './layouts/home/Home'
@@ -14,7 +14,8 @@ import Profile from './user/layouts/profile/Profile'
 
 // Redux Store
 import store from './store'
-
+const PUBLIC_URL = process.env.NODE_ENV === 'production' ? '/TheWill' : '/'
+const browserHistory = useRouterHistory(createHistory)({ basename: PUBLIC_URL })
 const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render((
